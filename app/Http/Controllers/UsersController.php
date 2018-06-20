75a7a95817be9b99a49301149a3b7329adf27626
disk <disk.sg.git@gmail.com>
2018-06-20 15:00:30 +0000

    user pages

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(10);
+
+        return veiw('user.index', [
+            'users' =>$users,
+    ]);
+    }
+    public function show($id)
+    {
+        $user = User::find($id);
+
+        return view('users.show', [
+            'user' => $user,
+        ]);
+    }
+}
